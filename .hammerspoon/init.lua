9ce878bb9fd8ae6b191f1de25ab3fc6d1b9c581c
Peter Lukacs <lukacs.peter.andras@gmail.com>
2021-05-04 11:00:37 +0200

    add firefox hotkeys, gitmoji, translation, notification fix to hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,4 +1,3 @@
-
 require('dual_caps')
 require('watcher')
 require('caffeinate')
@@ -6,5 +5,29 @@ require('window_movements')
 require('app_hotkeys')
 require('open_url')
 require('window_focus')
+require('firefox-setup')
+hs.loadSpoon("PopupTranslateSelection")
+hs.loadSpoon('Emojis')
 
-hs.notify.new({title="Hammerspoon", informativeText="Config loaded"}):send():release()
+-- Toggling the emoji chooser panel
+
+spoon.Emojis:bindHotkeys({
+    toggle = { {'ctrl', 'alt', 'cmd'}, "h" }
+})
+
+hyper:bind({}, 'h', function()
+  hs.eventtap.keyStroke({'cmd','alt','ctrl'}, 'h')
+  hyper.triggered = true
+end)
+
+-- spoon.PopupTranslateSelection:bindHotkeys(
+--    {
+--     translate_to_en = { { "ctrl", "alt", "cmd" }, "e" },
+--     translate_to_de = { { "ctrl", "alt", "cmd" }, "d" },
+--     translate_to_es = { { "ctrl", "alt", "cmd" }, "s" },
+--     translate_de_en = { { "shift", "ctrl", "alt", "cmd" }, "e" },
+--     translate_en_de = { { "shift", "ctrl", "alt", "cmd" }, "d" },
+--  }
+-- )
+
+hs.notify.new({title="Hammerspoon", informativeText="Config loaded"}):send()
