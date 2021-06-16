131ea1e05cbb919bf7ff701c3fd0594da0a86160
Peter Lukacs <lukacs.peter.andras@gmail.com>
2021-06-16 07:43:07 +0100

    remove stale code

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -7,9 +7,4 @@ require('open_url')
 require('window_focus')
 hs.loadSpoon("PopupTranslateSelection")
 
-hyper:bind({}, 'h', function()
-  hs.eventtap.keyStroke({'cmd','alt','ctrl'}, 'h')
-  hyper.triggered = true
-end)
-
 hs.notify.new({title="Hammerspoon", informativeText="Config loaded"}):send()
