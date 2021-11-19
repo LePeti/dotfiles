b6d4ba1b8c1f5ed1537eee8b77892daf341dc09e
Peter Lukacs <lukacs.peter.andras@gmail.com>
2021-11-19 11:14:07 +0000

    re-assign jkli letters to arrows

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -20,4 +20,21 @@ spoon.Emojis:bindHotkeys({
 })
 ---
 
+-- Rebind jkli to arrows
+arrowKey = function(arrow, modifiers)
+    local event = require("hs.eventtap").event
+    event.newKeyEvent(modifiers, string.lower(arrow), true):post()
+    event.newKeyEvent(modifiers, string.lower(arrow), false):post()
+  end
+
+  hyper:bind({}, 'j', function() arrowKey('LEFT', {}); end, nil, function() arrowKey('LEFT', {}); end)
+  hyper:bind({}, 'k', function() arrowKey('DOWN', {}); end, nil, function() arrowKey('DOWN', {}); end)
+  hyper:bind({}, 'i', function() arrowKey('UP', {}); end, nil, function() arrowKey('UP', {}); end)
+  hyper:bind({}, 'l', function() arrowKey('RIGHT', {}); end, nil, function() arrowKey('RIGHT', {}); end)
+  hyper:bind({'cmd'}, 'j', function() arrowKey('LEFT', {'cmd'}); end, nil, function() arrowKey('LEFT', {'cmd'}); end)
+  hyper:bind({'cmd'}, 'k', function() arrowKey('DOWN', {'cmd'}); end, nil, function() arrowKey('DOWN', {'cmd'}); end)
+  hyper:bind({'cmd'}, 'l', function() arrowKey('RIGHT', {'cmd'}); end, nil, function() arrowKey('RIGHT', {'cmd'}); end)
+  hyper:bind({'cmd'}, 'i', function() arrowKey('UP', {'cmd'}); end, nil, function() arrowKey('UP', {'cmd'}); end)
+
+
 hs.alert.show('Config loaded')
