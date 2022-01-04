cc57050276c6e90dc5df4f71323ac7b7a02d3715
Peter Lukacs <lukacs.peter.andras@gmail.com>
2022-01-04 13:56:33 +0100

    set microphone back to laptop's when connecting to bluetooth headphones

--- .hammerspoon/sound_output_device_change_notif.lua
+++ .hammerspoon/sound_output_device_change_notif.lua
@@ -5,6 +5,9 @@ function audioDeviceChanged(arg)
         lastSetDeviceTime = os.time()
     elseif arg == 'dOut' and os.time() - lastSetDeviceTime < 2 then
         hs.alert.show("Current Speaker: " .. hs.audiodevice.current().name)
+        mbp_mic = hs.audiodevice.findDeviceByName('MacBook Pro Microphone')
+        mbp_mic:setDefaultInputDevice()
+        hs.alert.show("Current Mic: " .. hs.audiodevice.defaultInputDevice():name())
     end
 end
 
