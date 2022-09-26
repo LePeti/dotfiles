30bd7b3a1273e4702b0707cfa423992f86acaabd
Peter Lukacs <lukacs.peter.andras@gmail.com>
2022-09-26 21:39:52 +0200

    add google calendar shortcut

--- .hammerspoon/chrome-tab-hotkeys.lua
+++ .hammerspoon/chrome-tab-hotkeys.lua
@@ -1,3 +1,7 @@
 hyper:bind({}, 'r', function()
     hs.osascript.applescriptFromFile('open-messenger-chrome.applescript')
 end)
+
+hyper:bind({}, 'g', function()
+    hs.osascript.applescriptFromFile('open-calendar-chrome.applescript')
+end)
