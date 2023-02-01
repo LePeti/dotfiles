ff91852eeca6580777a48087480c761e25cb1e9d
Peter Lukacs <lukacs.peter.andras@gmail.com>
2023-02-01 09:08:51 +0100

    add code comments

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -12,11 +12,12 @@ require('sound_output_device_change_notif')
 hs.loadSpoon('PopupTranslateSelection')
 hs.loadSpoon('Emojis')
 
--- hammerspoon reload & console
+-- reload hammerspoon
 hyper:bind({}, 'p', function()
     hs.reload()
 end)
 
+-- Open hammerspoon console
 hyper:bind({}, 'e', function()
     hs.toggleConsole()
 end)
@@ -31,6 +32,7 @@ spoon.Emojis:bindHotkeys({
     toggle = {{'ctrl', 'alt', 'cmd'}, 'h'}
 })
 
+-- bind 'a' as cmd + shift + m --> switch user in chrome
 hyper:bind({}, 'a', function()
     hs.eventtap.keyStroke({'cmd', 'shift'}, 'm')
     hs.eventtap.keyStroke({}, 'return')
@@ -49,9 +51,19 @@ hyper:bind({}, "q", function()
     hs.audiodevice.defaultOutputDevice():setVolume(0)
 end)
 
+
+-- Print audio input / output
 hyper:bind({}, 'n', function()
     hs.alert.show("Input:      " .. hs.audiodevice.defaultInputDevice():name() .. "\n" .. "Output:  " ..
                       hs.audiodevice.current().name)
+end)
+
+-- open aws tunnel
+hyper:bind({}, 'n', function()
+    -- the below doesn't work. also, using hs.task shouldn't be used for never exiting tasks
+    -- hs.execute("aws sso login; ssh -l 5439:main.ci6pf3wvc9tf.eu-west-1.redshift.amazonaws.com:5439 komoot-analytics-ssh-jump-host -n serveraliveinterval=60")
+    -- hs.application.launchorfocus('iterm')
+    -- hs.eventtap.keystrokes("asd")
 end)
 
 -- HS loaded notification
